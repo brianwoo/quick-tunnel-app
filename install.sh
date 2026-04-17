6325296775e362cc76828cfa5148a87f99f85fbe
Brian Woo <woo.brian@gmail.com>
2026-04-17 10:54:23 -0600

    Update data.7z extraction path in install.sh
    Change extraction path for data.7z to the user's home directory.

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
-pkg update
-pkg install stunnel p7zip
-
 if [ -e "$HOME/quick-tunnel" ]; then
   echo "quick-tunnel already exists, not overwriting..."
   exit 0
 fi
+
+pkg update
+pkg install stunnel p7zip
 
 #curl and save the file as data.7z
 curl https://raw.githubusercontent.com/brianwoo/quick-tunnel-app/refs/heads/main/data.7z -o data.7z
 
 read -s -p "Enter Family Password: " password < /dev/tty
 echo ""
-7z x "data.7z" -p"$password" -y
+7z x "data.7z" -p"$password" -o"$HOME" -y
 
 if [ -f "$HOME/data.7z" ]; then
   rm -f "$HOME/data.7z"
