62a92e221e1d682d8980e0f55cc8c9c86c991f0a
Brian Woo <woo.brian@gmail.com>
2026-04-17 10:31:16 -0600

    Add check for existing quick-tunnel directory
    Check for existing quick-tunnel directory before installation and notify user.

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 pkg update
 pkg install stunnel p7zip
+
+if [ -e "$HOME/quick-tunnel" ]; then
+  echo "quick-tunnel already exists, not overwriting..."
+  exit 0
+fi
+
 #curl and save the file as data.7z
+curl https://raw.githubusercontent.com/brianwoo/quick-tunnel-app/refs/heads/main/data.7z -o data.7z
 7z x data.7z
-cd quick-tunnel
+if [ -f "data.7z" ]; then
+  rm -f data.7z
+fi
 echo "install complete. cd quick-tunnel to start"
